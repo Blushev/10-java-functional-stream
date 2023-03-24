563c1b721b16c49d45da592b4af0e347f1787d2b
Egoru <egor.rumyancev.9494@mail.ru>
2023-03-24 21:46:47 +0500

    Румянцев Егор. module 10

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -23,5 +24,16 @@ public class Task03Main {
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
         // your implementation here
+        final Iterator<? extends T> iterator = stream.iterator();
+        T min = iterator.hasNext() ? iterator.next() : null;
+        T max = min;
+
+        while (iterator.hasNext()){
+            T element = iterator.next();
+            if(order.compare(element, min) < 0) min = element;
+            else if(order.compare(element, max) > 0) max = element;
+        }
+
+        minMaxConsumer.accept(min, max);
     }
 }
